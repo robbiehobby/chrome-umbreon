103b9d9ee24d17dca96c08855b0aaf027a72724c
Rob <i942tb@ionsnare.com>
2025-05-15 23:57:13 -0400

    Fixed missing overlay element on certain websites

--- src/features/overlay/extension/content.ts
+++ src/features/overlay/extension/content.ts
@@ -1,8 +1,32 @@
 (function () {
   let overlay: HTMLElement | null = document.querySelector("umbreon-overlay");
 
+  async function create() {
+    overlay = document.createElement("umbreon-overlay") as HTMLElement;
+    overlay.style.position = "fixed";
+    overlay.style.zIndex = "2147483647";
+    overlay.style.inset = "0";
+    overlay.style.pointerEvents = "none";
+    overlay.style.visibility = "hidden";
+    overlay.style.opacity = "0";
+    overlay.style.backgroundColor = "#000";
+    overlay.style.mixBlendMode = "multiply";
+    document.documentElement.appendChild(overlay);
+  }
+
+  async function load() {
+    let settings: Settings | null = null;
+    try {
+      // Try to load the settings from local storage first.
+      settings = JSON.parse(localStorage.getItem("umbreon-overlay") || "");
+    } catch (_error) {}
+    if (!settings) settings = await chrome.runtime.sendMessage({ type: "getSettings" });
+    if (settings) update(settings);
+  }
+
   function update(settings: Settings, type: UpdateType = null) {
     if (!overlay) return;
+    if (!document.documentElement.contains(overlay)) create();
     const mode = settings[settings.website.mode];
 
     if (type === "activated") overlay.style.transitionProperty = "none";
@@ -19,29 +43,8 @@
     }, 200);
   }
 
-  async function create() {
-    overlay = document.createElement("umbreon-overlay") as HTMLElement;
-    overlay.style.position = "fixed";
-    overlay.style.zIndex = "2147483647";
-    overlay.style.inset = "0";
-    overlay.style.pointerEvents = "none";
-    overlay.style.visibility = "hidden";
-    overlay.style.opacity = "0";
-    overlay.style.backgroundColor = "#000";
-    overlay.style.mixBlendMode = "multiply";
-    document.documentElement.appendChild(overlay);
-
-    // Try to load the settings from local storage first.
-    let settings: Settings | null = null;
-    try {
-      settings = JSON.parse(localStorage.getItem("umbreon-overlay") || "");
-    } catch (_error) {}
-    if (!settings) {
-      settings = await chrome.runtime.sendMessage({ type: "getSettings" });
-    }
-    if (settings) update(settings);
-  }
-  if (!overlay) create();
+  create();
+  load();
 
   chrome.runtime.onMessage.addListener((message) => {
     if (message.action === "update") {
